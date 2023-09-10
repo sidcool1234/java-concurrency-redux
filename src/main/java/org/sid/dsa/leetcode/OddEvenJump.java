be6ec5e9c78e5393be1b9b4ca62a7eecd1c65cc9
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2023-09-10 13:30:46 +0530

    Odd Even Jump - Added null condition

--- src/main/java/org/sid/dsa/leetcode/OddEvenJump.java
+++ src/main/java/org/sid/dsa/leetcode/OddEvenJump.java
@@ -1,13 +1,17 @@
 package org.sid.dsa.leetcode;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 import java.util.TreeMap;
+
 // https://leetcode.com/problems/odd-even-jump/description/
 public class OddEvenJump {
 
     // This is copy pasted.  But I understood the gist
     public static int oddEvenJumps(int[] A) {
-        int n  = A.length, res = 1;
+        if (A == null || A.length == 0) return 0;
+        int n = A.length, res = 1;
         boolean[] higher = new boolean[n], lower = new boolean[n];
         higher[n - 1] = lower[n - 1] = true;
         TreeMap<Integer, Integer> map = new TreeMap<>();
